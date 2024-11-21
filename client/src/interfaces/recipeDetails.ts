cd719216c76f26111f364ae2d8c0cf279a04ff44
Caryn Carter <carter.caryn@gmail.com>
2024-11-21 16:33:15 -0600

    fixed the interfaces on this

--- client/src/interfaces/recipeDetails.ts
+++ client/src/interfaces/recipeDetails.ts
@@ -6,15 +6,9 @@ export default interface RecipeDetails {
     servings: number,
     ingredients: string[],
     instructions: string,
-<<<<<<< HEAD
     steps: string[] | null,
     diets?: string[]| null,
     image?: string | null,
-=======
-    steps: string[]| null,
-    diets: string[]| null,
-    image: string | null,
->>>>>>> 8da65e991d4aa6652e2b353fcab6c07447f4ac94
     sourceUrl?: string| null,
     spoonacularSourceUrl?: string| null,
     spoonacularId?: number| null,
